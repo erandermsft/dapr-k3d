01dbaede121b7d10be6e2fe1e10d3ea78c731fbe
bartr <bartr@microsoft.com>
2021-08-12 03:54:13 +0000

    updated dev container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,7 @@ echo "post-create start" >> ~/status
 # this runs in background after UI is available
 
 # (optional) upgrade packages
-sudo apt-get update
+#sudo apt-get update
 #sudo apt-get upgrade -y
 #sudo apt-get autoremove -y
 #sudo apt-get clean -y
